a76db7796acb079536674b381e32541c46bee120
Wandering_Sta71 <1481212714@qq.com>
2024-05-13 16:31:10 +0800

    test-1

--- test2.py
+++ test2.py
@@ -14,9 +14,10 @@ y = iris.data.targets
 
 # splitting X and y into training and testing sets
 from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=1)
-
+'''
 # training the model on training set
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(n_neighbors=3)
@@ -24,7 +25,7 @@ knn.fit(X_train, y_train)
 
 # making predictions on the testing set
 y_pred = knn.predict(X_test)
-
+'''
 '''
 import sklearn.neural_network
 # Create an instance of the MLPClassifier class
@@ -32,6 +33,14 @@ neural_network = sklearn.neural_network.MLPClassifier(hidden_layer_sizes=(10, 10
 neural_network.fit(X_train, y_train)
 y_pred = neural_network.predict(X_test)
 '''
+
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
 # comparing actual response values (y_test) with predicted response values (y_pred)
 from sklearn import metrics
+
 print("KNN model accuracy", metrics.accuracy_score(y_test, y_pred))
